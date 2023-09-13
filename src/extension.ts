8aeff5d929b77e3a50e9eca9fbd9ce61029b0491
liuyao <liuyao@ft.com>
2023-09-13 11:23:52 +0800

    feat: add showNowTimeCommand

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const showNowTimeCommand = vscode.commands.registerCommand(
+  "vscode_extension.showNowTime",
+  () => {
+    const date = new Date().toLocaleDateString();
+    const time = new Date().toLocaleTimeString();
+
+    const items = [
+      `${date.replace(/\//g, "-")} ${time}`,
+      `${date} ${time}`,
+      date,
+      date.replace(/\//g, "-"),
+      time,
+    ];
+
+    vscode.window.showQuickPick(items).then((selectedItem) => {
+      if (selectedItem) {
+        // 处理用户选择的项目
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+          // 将选项内容插入到文档中
+          editor.edit((editBuilder) => {
+            editBuilder.insert(editor.selection.active, selectedItem);
+          });
+        } else {
+          vscode.window.showErrorMessage("No active text editor.");
+        }
+      }
+    });
+  }
+);
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-extension" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode_extension!');
-	});
-
-	context.subscriptions.push(disposable);
+  console.log(
+    'Congratulations, your extension "vscode-extension" is now active!'
+  );
+  context.subscriptions.push(showNowTimeCommand);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
